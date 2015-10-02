c4c28c9ab144844f6120dd06292aae590f193ac4
Neil Magee <info@neilmagee.com>
2015-10-02 23:06:01 +0100

    API interaction optimisations
    file_get_contents was taking a long time to get the 60 stories

--- cron/gethn.php
+++ cron/gethn.php
@@ -1,6 +1,4 @@
 <?php
-set_time_limit(60);
-
 /*******************************************************************************
  * VARIABLES
  ******************************************************************************/
@@ -28,21 +26,37 @@ if (file_exists($filename)) {
  * FUNCTIONS
  ******************************************************************************/
 
+/**
+ * [get_list get list of top Hacker News stories via API]
+ * @return [array] [list of top story IDs]
+ */
 function get_list() {
-    $list = file_get_contents('https://hacker-news.firebaseio.com/v0/topstories.json');
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://hacker-news.firebaseio.com/v0/topstories.json');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $list = curl_exec($ch);
     $list_obj = json_decode($list);
     $list = object_to_array($list_obj);
 
     return $list;
 }
 
+/**
+ * [save_article_list Cycle through top HN stories and get each set of data]
+ * @param  [array] $output [list of top story data]
+ * @param  [array] $source [list of top story IDs]
+ * @param  [str] $dir    [absolute path]
+ */
 function save_article_list($output, $source, $dir) {
     $count = 0;
     $limit = 60;
+    $ch = curl_init();
 
     for ($count; $count < $limit; $count++) {
         $id = $source[$count];
-        $article = file_get_contents('https://hacker-news.firebaseio.com/v0/item/' . $id . '.json');
+        curl_setopt($ch, CURLOPT_URL, 'https://hacker-news.firebaseio.com/v0/item/' . $id . '.json');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $article = curl_exec($ch);
         $article_obj = json_decode($article);
         $output[] = object_to_array($article_obj);
     }
@@ -51,16 +65,31 @@ function save_article_list($output, $source, $dir) {
     file_put_contents($dir . '/../src/data/data.json', $json_data);
 }
 
+/**
+ * [create_file if file does not exist, create it]
+ * @param  [str] $dir [path to place file]
+ * @param  [str] $n   [file name]
+ * @param  [str] $xt  [file extension]
+ */
 function create_file($dir, $n, $xt) {
     fopen($dir . $n . '.' . $xt, "w");
 }
 
+/**
+ * [pre_r pretty print]
+ * @param  [array or object] $val [source data to be printed]
+ */
 function pre_r($val){
     echo '<pre>';
     print_r($val);
     echo  '</pre>';
 }
 
+/**
+ * [object_to_array does what it says on the tin]
+ * @param  [object] $d [input object]
+ * @return [array]    [output array]
+ */
 function object_to_array($d) {
     if (is_object($d)) {
         // Gets the properties of the given object
