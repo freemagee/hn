5b1420e672fff4c7dd7c74d1e4070eb5b4a06620
Neil Magee <info@neilmagee.com>
2019-01-04 11:41:35 +0000

    Reformat cron php using phpcbf

--- cron/getcomments.php
+++ cron/getcomments.php
@@ -1,33 +1,33 @@
 <?php
-/*******************************************************************************
-* INCLUDES
- ******************************************************************************/
+/*
+ * INCLUDES
+ */
 
-include_once(realpath(__DIR__ . '/..') . '/src/inc/common_functions.php');
+require_once realpath(__DIR__.'/..').'/src/inc/common_functions.php';
 
-/*******************************************************************************
+/*
  * VARIABLES
- ******************************************************************************/
+ */
 
-$dir = realpath(__DIR__ . '/..') . '/src/data/';
-$source_file = $dir . 'articles.json';
-$output_file = $dir . 'comments.json';
+$dir         = realpath(__DIR__.'/..').'/src/data/';
+$source_file = $dir.'articles.json';
+$output_file = $dir.'comments.json';
 
 if (!file_exists($output_file)) {
     create_file($dir, 'comments', 'json');
 }
 
-$source_data = file_get_contents($source_file);
-$source_obj = json_decode($source_data);
+$source_data     = file_get_contents($source_file);
+$source_obj      = json_decode($source_data);
 $hn_article_list = object_to_array($source_obj);
 
-/******************************************************************************
+/*
  * LOGIC
- *****************************************************************************/
+ */
 
 if (!empty($hn_article_list)) {
-    $comments = array();
-    $limit = count($hn_article_list);
+    $comments = [];
+    $limit    = count($hn_article_list);
 
     for ($i = 0; $i < $limit; $i++) {
         $id = $hn_article_list[$i]['id'];
@@ -38,45 +38,54 @@ if (!empty($hn_article_list)) {
     }
 
     if (!empty($comments)) {
-        //$time_start = microtime(true);
+        // $time_start = microtime(true);
         $comments_list = generate_comment_list($comments);
-        //echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
+        // echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
         $json_data = json_encode($comments_list);
         file_put_contents($output_file, $json_data);
     }
 }
 
-/*******************************************************************************
+/*
  * FUNCTIONS
- ******************************************************************************/
+ */
 
-function generate_comment_url($id) {
-    //return 'https://hacker-news.firebaseio.com/v0/item/' . $id . '.json';
+
+function generate_comment_url($id)
+{
+    // return 'https://hacker-news.firebaseio.com/v0/item/' . $id . '.json';
     // Now using an alternative unofficial API
-    return 'http://node-hnapi.herokuapp.com/item/' .$id;
-}
+    return 'http://node-hnapi.herokuapp.com/item/'.$id;
 
-function generate_comment_list($array) {
-    $output = array();
-    $limit = count($array);
+}//end generate_comment_url()
+
+
+function generate_comment_list($array)
+{
+    $output = [];
+    $limit  = count($array);
 
     for ($i = 0; $i < $limit; $i++) {
-        $id = $array[$i];
-        $url = generate_comment_url($id);
-        $comment = get_comment($url);
+        $id          = $array[$i];
+        $url         = generate_comment_url($id);
+        $comment     = get_comment($url);
         $output[$id] = $comment;
     }
 
     return $output;
-}
 
-function get_comment($url) {
+}//end generate_comment_list()
+
+
+function get_comment($url)
+{
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $data = curl_exec($ch);
+    $data     = curl_exec($ch);
     $data_obj = json_decode($data);
-    $comment = object_to_array($data_obj);
+    $comment  = object_to_array($data_obj);
 
     return $comment;
-}
+
+}//end get_comment()
--- cron/gethn.php
+++ cron/gethn.php
@@ -1,23 +1,23 @@
 <?php
-/*******************************************************************************
-* INCLUDES
- ******************************************************************************/
+/*
+ * INCLUDES
+ */
 
-include_once(realpath(__DIR__ . '/..') . '/src/inc/common_functions.php');
+require_once realpath(__DIR__.'/..').'/src/inc/common_functions.php';
 
-/*******************************************************************************
+/*
  * VARIABLES
- ******************************************************************************/
+ */
 
 $articles = [];
-$dir = realpath(__DIR__ . '/..') . '/src/data/';
-$file = 'articles.json';
-$filename = $dir . $file;
+$dir      = realpath(__DIR__.'/..').'/src/data/';
+$file     = 'articles.json';
+$filename = $dir.$file;
 $top_list = '';
 
-/*******************************************************************************
+/*
  * LOGIC
- ******************************************************************************/
+ */
 
 if (file_exists($filename)) {
     $top_list = get_list();
@@ -28,45 +28,53 @@ if (file_exists($filename)) {
     save_article_list($articles, $top_list, $dir);
 }
 
-/*******************************************************************************
+/*
  * FUNCTIONS
- ******************************************************************************/
+ */
+
 
 /**
- * [get_list get list of top Hacker News stories via API]
- * @return [array] [list of top story IDs]
+ * Get list of top Hacker News stories via API
+ *
+ * @return array list of top story IDs
  */
-function get_list() {
+function get_list()
+{
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://hacker-news.firebaseio.com/v0/topstories.json');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $list = curl_exec($ch);
+    $list     = curl_exec($ch);
     $list_obj = json_decode($list);
-    $list = object_to_array($list_obj);
+    $list     = object_to_array($list_obj);
 
     return $list;
-}
+
+}//end get_list()
+
 
 /**
- * [save_article_list Cycle through top HN stories and get each set of data]
- * @param  [array] $output [list of top story data]
- * @param  [array] $source [list of top story IDs]
- * @param  [str] $dir    [absolute path]
+ * Cycle through top HN stories and get each set of data
+ *
+ * @param array  $output list of top story data
+ * @param array  $source list of top story IDs
+ * @param string $dir    absolute path
  */
-function save_article_list($output, $source, $dir) {
+function save_article_list($output, $source, $dir)
+{
     $count = 0;
     $limit = 60;
-    $ch = curl_init();
+    $ch    = curl_init();
 
     for ($count; $count < $limit; $count++) {
         $id = $source[$count];
-        curl_setopt($ch, CURLOPT_URL, 'https://hacker-news.firebaseio.com/v0/item/' . $id . '.json');
+        curl_setopt($ch, CURLOPT_URL, 'https://hacker-news.firebaseio.com/v0/item/'.$id.'.json');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $article = curl_exec($ch);
+        $article     = curl_exec($ch);
         $article_obj = json_decode($article);
-        $output[] = object_to_array($article_obj);
+        $output[]    = object_to_array($article_obj);
     }
 
     $json_data = json_encode($output);
-    file_put_contents($dir . '/articles.json', $json_data);
-}
+    file_put_contents($dir.'/articles.json', $json_data);
+
+}//end save_article_list()
