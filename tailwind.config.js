9a165ba709fa480403b0586fa81ce3aa13d5d015
freemagee <1529405+freemagee@users.noreply.github.com>
2020-05-16 23:13:40 +0100

    Add dark mode for comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -156,6 +156,7 @@ module.exports = {
         borderWidth: {
             default: "1px",
             "0": "0",
+            "2": "2px",
             code: "4px",
         },
         transitionDuration: {
